0208d4c10d0abf471105b6a12335a0bf9d2e7956
Socheat Sok <alex@socheat.net>
2021-10-14 12:24:27 +0700

    feat: init project

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+name="nginx-s6-overlay"
+builds=`ls builds`
+
+for version in ${builds[*]}; do
+    echo "Building ${name}:${version}"
+    docker build --pull --rm -f "builds/$version/Dockerfile" -t ${name}:${version} .
+done
---
+++ render.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+tags=(mainline alpine)
+vers=(1.21 1.21.3 1.21-alpine 1.21.3-alpine)
+
+render() {
+  sedStr="s!%%NGINX_VERSION%%!$1!g;"
+  sed -r "$sedStr" $2
+}
+
+generate() {
+  local versions=$@
+  for version in ${versions[*]}; do
+    echo "Genetating nginx:$version"
+    # If exist, cleanup
+    if [ -d "builds/$version" ]; then
+      rm -rf "builds/$version";
+    fi
+
+    mkdir -p "builds/$version"
+    # cp -r rootfs "builds/$version"
+    render $version Dockerfile.template > "builds/$version/Dockerfile"
+  done
+}
+
+generate ${tags[@]}
+generate ${vers[@]}
