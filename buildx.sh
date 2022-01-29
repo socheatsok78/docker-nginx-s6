1fc87c0b773919fa6d02d2126e4aa80ffd9b2e74
Socheat Sok <alex@socheat.net>
2022-01-29 17:25:06 +0700

    feat: enable docker command

--- buildx.sh
+++ buildx.sh
@@ -8,24 +8,24 @@ DOCKER_IMAGE_NAME=socheatsok78/nginx-s6
 BUILDX_PLATFORM=linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6
 
 echo Building multi-arch docker image
-# docker buildx rm ${DOCKER_IMAGE_BUILDER}-builder || true
-# docker run --rm \
-#     --privileged tonistiigi/binfmt:latest \
-#     --install all
-# docker buildx create \
-#     --name ${DOCKER_IMAGE_BUILDER}-builder \
-#     --driver docker-container \
-#     --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host' \
-#     --use
-# docker buildx inspect --bootstrap
+docker buildx rm ${DOCKER_IMAGE_BUILDER}-builder || true
+docker run --rm \
+    --privileged tonistiigi/binfmt:latest \
+    --install all
+docker buildx create \
+    --name ${DOCKER_IMAGE_BUILDER}-builder \
+    --driver docker-container \
+    --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host' \
+    --use
+docker buildx inspect --bootstrap
 for version in "${builds[@]}"; do
     echo "==> Building ${DOCKER_IMAGE_NAME}:${version}"
-    # docker buildx build \
-    #     --platform=${BUILDX_PLATFORM} \
-    #     --tag ${DOCKER_IMAGE_NAME}:${version} \
-    #     --push \
-    #     --file "builds/$version/Dockerfile" .
+    docker buildx build \
+        --platform=${BUILDX_PLATFORM} \
+        --tag ${DOCKER_IMAGE_NAME}:${version} \
+        --push \
+        --file "builds/$version/Dockerfile" .
     echo "==> [Done] Successfully built ${DOCKER_IMAGE_NAME}:${version}"
     echo
 done
-# docker buildx rm ${DOCKER_IMAGE_BUILDER}-builder
+docker buildx rm ${DOCKER_IMAGE_BUILDER}-builder
